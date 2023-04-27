39b0470c328778355d0da05c5a51c34b4d323e3b
Rox0617 <100675378+Rox0617@users.noreply.github.com>
2023-04-27 18:04:44 +0800

    Add files via upload

---
+++ spring-cloud weather forecasting/DEMO_service/city_server/src/main/java/com/demo/city/CityServerApplication.java
@@ -0,0 +1,17 @@
+package com.demo.city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @desc 城市服务启动类
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class CityServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CityServerApplication.class,args);
+    }
+
+}
---
+++ spring-cloud weather forecasting/DEMO_service/test_server/src/main/java/com/demo/test/TestServerApplication.java
@@ -0,0 +1,17 @@
+package com.demo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @desc测试服务启动类
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class TestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestServerApplication.class,args);
+    }
+}
